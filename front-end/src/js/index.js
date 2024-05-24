e2aee6e3f65a3cdf000ff16c07a0dea7b7c3d407
sagar sonawane <sagar.sonawane@espressif.com>
2024-05-24 14:23:45 +0530

    Task: update website content

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+const mobileNavBtn = document.getElementById('mobile-nav-btn')
+const mobileNav = document.getElementById('mobile-nav')
+
+mobileNavBtn.addEventListener('click', (e) => {
+    if(mobileNav.style.height === '')
+    {
+        mobileNav.style.height = 'fit-content'
+    }else{
+        mobileNav.style.height = ''
+    }
+})
